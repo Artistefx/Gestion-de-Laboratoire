c8fef0fabb9e88d8878d142bb4018be108a096ee
Artistefx <114290288+Artistefx@users.noreply.github.com>
2024-10-23 17:46:14 +0100

    Microservices structure added

---
+++ backend/LaboMicroServices/Laboratoire/src/test/java/org/ensa/AppTest.java
@@ -0,0 +1,19 @@
+package org.ensa;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
